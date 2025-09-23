54c32ae86718df19d71c73b0abdf585785e3881a
Matheus Alves <matheus.alves@factoryofidea.com.br>
2025-09-23 14:53:04 -0300

    refact: Adcionado verificação na ui de pokemon evolutions em casos de pokemons que não possuem evolução mas possuem sua propria referencia de evolução e também casos onde os pokemons não possuem nenhuma referencia.

--- lib/ui/pokemon_details/widgets/pokemon_evolutions.dart
+++ lib/ui/pokemon_details/widgets/pokemon_evolutions.dart
@@ -14,17 +14,14 @@ class PokemonEvolutions extends StatelessWidget {
     final bool isDarkTheme = theme.brightness == Brightness.dark;
     return Padding(
       padding: const EdgeInsets.fromLTRB(12.0, 12.0, 12.0, 36.0),
-      child: Column(
-        spacing: 8.0,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text('Evoluções', style: Theme.of(context).textTheme.titleMedium),
-          pokemonDetailsViewmodel.pokemon.evolutionChain.isEmpty
-              ? Text(
-                  'Este pokémon não possui evoluções.',
-                  style: Theme.of(context).textTheme.labelSmall,
-                )
-              : Container(
+      child: pokemonDetailsViewmodel.pokemon.evolutionChain.isEmpty
+          ? SizedBox.shrink()
+          : Column(
+              spacing: 8.0,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Evoluções', style: Theme.of(context).textTheme.titleMedium),
+                Container(
                   padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 16.0),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(15.0),
@@ -39,41 +36,53 @@ class PokemonEvolutions extends StatelessWidget {
                       index,
                     ) {
                       final evolutions = pokemonDetailsViewmodel.pokemon.evolutionChain;
+                      final hasEvolutions = evolutions.length > 1;
                       return Column(
                         spacing: 16.0,
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
+                          hasEvolutions
+                              ? SizedBox.shrink()
+                              : Text(
+                                  'Este Pokémon não evolui.',
+                                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                    color: Theme.of(context).colorScheme.secondary,
+                                  ),
+                                ),
                           PokemonEvolutionCard(
                             evolutionStep: evolutions[index],
                             pokemonCurrentName: pokemonDetailsViewmodel.pokemon.name,
                           ),
-                          Row(
-                            spacing: 8.0,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              index == evolutions.length - 1
-                                  ? Container()
-                                  : SvgPicture.asset(
-                                      'assets/icons/svg/pokemon_species/arrow_down_icon.svg',
-                                      colorFilter: ColorFilter.mode(
-                                        isDarkTheme ? AppColors.white1 : AppColors.darkblue,
-                                        BlendMode.srcIn,
+                          hasEvolutions
+                              ? Row(
+                                  spacing: 8.0,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    index == evolutions.length - 1
+                                        ? Container()
+                                        : SvgPicture.asset(
+                                            'assets/icons/svg/pokemon_species/arrow_down_icon.svg',
+                                            colorFilter: ColorFilter.mode(
+                                              isDarkTheme ? AppColors.white1 : AppColors.darkblue,
+                                              BlendMode.srcIn,
+                                            ),
+                                          ),
+                                    Text(
+                                      evolutions[index].triggerDescription ?? 'Initial',
+                                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                                        color: isDarkTheme ? AppColors.white1 : AppColors.darkblue,
                                       ),
                                     ),
-                              Text(
-                                evolutions[index].triggerDescription ?? 'Initial',
-                                style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                                  color: isDarkTheme ? AppColors.white1 : AppColors.darkblue,
-                                ),
-                              ),
-                            ],
-                          ),
+                                  ],
+                                )
+                              : SizedBox.shrink(),
                         ],
                       );
                     }),
                   ),
                 ),
-        ],
-      ),
+              ],
+            ),
     );
   }
 }
